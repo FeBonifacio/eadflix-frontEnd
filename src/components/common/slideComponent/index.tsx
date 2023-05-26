3f2b04c682bcbe00b04da24043a41081e1b9bd81
FeBonifacio <103700361+FeBonifacio@users.noreply.github.com>
2023-05-26 11:31:20 -0300

    Criando responsividade e estilizando a tela do slide

--- src/components/common/slideComponent/index.tsx
+++ src/components/common/slideComponent/index.tsx
@@ -11,14 +11,44 @@ interface props {
 // Isso é novo para mim, peguei uma aula na onebitcode
 // Pegando o curso no back e colocando no front
 const SlideComponent = function ({ course }: props) {
+    let slideCount = 0;
+
+    if (course.length > 4) {
+        slideCount = 4;
+    } else if (course) {
+        slideCount = course.length;
+    }
+
     return (
     <>
-    <div>
+    <div className="d-flex flex-column align-items-center py-4">
         <Splide options={{
             type: "loop",
-            perPage: 4,
+            perPage: slideCount,
             perMove: 1,
+            // aqui é quando tiver só um curso na categoria
+            width: slideCount * 300,
             pagination: false,
+            arrows: course.length > 4 ? true : false,
+            drag: course.length > 4 ? true : false,
+            //responsividade
+            breakpoints: {
+                1200: {
+                    perPage: slideCount >= 2 ? 2 : 1,
+                    width: slideCount >= 2 ? 600 : 300,
+                    arrows: course.length > 2 ? true : false,
+                    drag: course.length > 2 ? true : false,
+                },
+                600: {
+                    perPage: 1,
+                    width: 300,
+                    arrows: course.length > 1 ? true : false,
+                    drag: course.length > 1 ? true : false,
+                },
+                300: {
+                    width: 250,
+                }
+            },
         }}>
             {course?.map((course)=>(
                 <SplideSlide key={course.id}>
